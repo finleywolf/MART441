f512fa865ad3cbaa0021901f94017f6651747336
finleywolferman <fw190358@umconnect.umt.edu>
2026-02-02 21:20:49 -0700

    building lil website

---
+++ HW3/script.js
@@ -0,0 +1,20 @@
+// favorite websites
+console.log("Favorite Websites")
+console.log("1. Youtube")
+console.log("2. Depop")
+console.log("3. Instagram")
+
+// favorite games 
+console.log("1. Minecraft")
+console.log("2. Wavelength")
+console.log("3. Wordle")
+
+// favorite artists
+console.log("1. Jeff Buckley - influenced many artists after him due to his raw vocals, vunerable songwriting, and poetic longing")
+console.log("2. David Lynch - captured surrealism in the medium of films, created many influential pieces of work that will be appriciated for years to come")
+console.log("3. Hayao Miyazaki - created Studio Ghibli and created many incredible animated works of art for all to enjoy ")
+
+// Question/Answer
+let userAnswer = prompt("What is your favorite website?")
+// Response
+console.log("User's favorite game:", userAnswer);
